54fd31bae058683bf2ce8e829f731eaf1d641cf1
LordBurned <pabloooas@gmail.com>
2025-02-09 14:41:02 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,1 +1,11 @@
+function toggleMobileMenu(){
+    document.getElementById("menu").style.width = "100%";
+    document.getElementById("close_bt").style.display = "flex";
+    document.getElementById("menu").style.transition = "0.3s";
+}
 
+function closeSideNav(){
+    document.getElementById("menu").style.width = "0";
+    document.getElementById("close_bt").style.display = "none";
+    document.getElementById("menu").style.transition = "0.3s";
+}
